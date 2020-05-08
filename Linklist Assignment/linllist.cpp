83004cdec1fddc57a5bdd1b625058bc5817e4ffb
AniketSonawane <64416668+AniketSonawane@users.noreply.github.com>
2020-05-08 18:11:40 +0530

    Add files via upload

---
+++ Linklist Assignment/linllist.cpp
@@ -0,0 +1,51 @@
+
+#include <iostream>
+using namespace std;
+  class Node
+    { 
+       public:
+       
+        int data; 
+        struct Node* next;
+        
+      
+}; 
+
+
+class linklist
+{
+    public:
+    void printList( Node* n) 
+{ 
+        while (n != NULL) { 
+                cout<<"  "<< n->data; 
+                n = n->next; 
+        } 
+} 
+};
+
+int main() 
+{ 
+    linklist l1;
+        Node *head = NULL; 
+         Node *second = NULL; 
+         Node *third = NULL; 
+
+        
+        head = new Node();
+        second = new Node();
+        third = new Node();
+
+        head->data = 1; 
+        head->next = second; 
+
+        second->data = 2;
+        second->next = third; 
+
+        third->data = 3;
+        third->next = NULL; 
+
+    l1. printList(head); 
+
+        return 0; 
+}
